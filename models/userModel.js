37ecf4f81b63ae047315c9d5e91a1bda09018876
dikxak <dikshakpoudel76@gmail.com>
2022-05-16 18:06:56 +0545

    complete model for user registration and login functionality

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserModel = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Customer', UserModel);
