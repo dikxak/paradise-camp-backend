6baca5a44730046c7569644fe4cf3317209cf346
dikxak <dikshakpoudel76@gmail.com>
2022-05-16 19:47:07 +0545

    import dotenv into app.js file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
 
 app.use(express.json());
@@ -7,4 +8,4 @@ require('./database/connection');
 
 app.use('/users', require('./routes/userRoutes'));
 
-app.listen(90);
+app.listen(process.env.PORT || 90);
