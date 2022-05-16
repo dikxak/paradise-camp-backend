6303ca3ea7a811a2ead77646fa1436ed96f98686
dikxak <dikshakpoudel76@gmail.com>
2022-05-16 19:23:12 +0545

    replace the exposed jwt secret with env value

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const User = require('../models/userModel');
 
 const registerUser = async (req, res) => {
@@ -74,7 +75,7 @@ const loginUser = async (req, res) => {
 
 // Generate JWT
 const generateToken = id => {
-  return jwt.sign({ id }, 'dikxak', {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
 };
