73d7b753f3dbe302e1b1e4ff6d41eabc998cde8e
dikxak <dikshakpoudel76@gmail.com>
2022-05-16 19:23:35 +0545

    replace exposed jwt with environment variable

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect('mongodb://127.0.0.1:27017/paradise_camp_db', {
+mongoose.connect(`${process.env.MONGO_URI}/paradise_camp_db`, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
