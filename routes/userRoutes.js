d1e28bb3b8b3fe009057ab721cdee7ce6a889697
dikxak <dikshakpoudel76@gmail.com>
2022-05-21 21:50:50 +0545

    add routes for adding and getting image

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = new express.Router();
+const { userGuard } = require('../auth/auth');
+const upload = require('../upload/imageUpload');
 
-const { registerUser, loginUser } = require('../controller/userController');
+const {
+  registerUser,
+  loginUser,
+  uploadLocationImage,
+  getLocationImage,
+} = require('../controller/userController');
 
 // Customer Register
 router.post('/register', registerUser);
 
 // Customer login
 router.post('/login', loginUser);
+
+// img - for postman
+router
+  .route('/picture/upload')
+  .put(userGuard, upload.single('img'), uploadLocationImage);
+
+router.route('/image/:filename.jpg').get(userGuard, getLocationImage);
 
 module.exports = router;
