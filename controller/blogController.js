c4e045e895b0c942a57db2c91d51a83a221901e9
dikxak <dikshakpoudel76@gmail.com>
2022-05-23 19:36:04 +0545

    add controller for blog and add code to add the blog

---
+++ controller/blogController.js
@@ -0,0 +1,45 @@
+const Blog = require('../models/blogModel');
+const User = require('../models/userModel');
+
+const addBlog = async (req, res) => {
+  try {
+    const user = req.user;
+
+    if (!user) {
+      res.status(400);
+      throw new Error('User not authorized.');
+    }
+
+    const { title, description, writtenDate, subtitle } = req.body;
+
+    if (!title || !description || !writtenDate) {
+      res.status(400);
+      throw new Error('Please fill the required fields.');
+    }
+
+    const blogData = await Blog.create({
+      title,
+      description,
+      writtenDate,
+      subtitle,
+      userId: req.user.id,
+    });
+
+    if (blogData) {
+      res.status(200);
+      res.json({
+        title: blogData.title,
+        description: blogData.description,
+        writtenDate: blogData.writtenDate,
+        userId: blogData.userId,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid blog data.');
+    }
+  } catch (err) {
+    res.json({ message: err.message, stack: err.stack });
+  }
+};
+
+module.exports = { addBlog };
