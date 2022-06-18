c40c0762898596b7043b9250e47f709692017c44
dikxak <dikshakpoudel76@gmail.com>
2022-06-18 14:02:42 +0545

    import cors and use

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 require('./database/connection');
 
